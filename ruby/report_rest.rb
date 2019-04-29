9708960111dc0ed598d9a45eda2b8b67705774c6
Luiz Carvalho <maximusmano@gmail.com>
2019-04-29 09:41:11 -0300

    Update report_rest.rb

--- ruby/report_rest.rb
+++ ruby/report_rest.rb
@@ -1,101 +1,114 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+#!/usr/bin/env ruby
+# encoding: utf-8
 
-require "rubygems"
-require "net/http"
-require "rexml/document"
+require 'rubygems'
+require 'net/http'
+require 'rexml/document'
+require 'json'
+require 'dotenv/load'
 
-REPORT_NAME = 'StandardChartsReport'
-REPORT_GROUP = "samples"
-SERVER_ROOT = "127.0.0.1" #Your Jasper Server Address
-SERVER_PORT = 8080 #Port of Jasper Server
-SERVER_USER = "jasperadmin" #Jasper Server Admin
-SERVER_PASSWORD = "jasperadmin" #JasperServer Password
-FORMAT = "PDF" #Format of output
-FILE_NAME = "report.pdf" #Name of output file
+@report_name = 'StandardChartsReport'
+@report_group = 'samples'
+SERVER_ROOT = ENV['JASPER_SERVER'] # Your Jasper Server Address
+SERVER_PORT = ENV['JASPER_PORT']
+SERVER_USER = ENV['JASPER_USER']
+SERVER_PASSWORD = ENV['JASPER_PASSWORD']
+DEBUG = false
+TIMEOUT = 3600 # seconds
 
+class Jasper
+  BASE_URL = 'http://%s:%s/jasperserver/rest/report' % [SERVER_ROOT, SERVER_PORT]
+  TIME_ZONE_PARAMS = 'userTimezone=America/Araguaina'.freeze
 
-REPORT_PATH = "http://#{SERVER_ROOT}:#{SERVER_PORT}/jasperserver/rest/report"
+  def initialize(report_group, report_name, params, format = 'pdf')
+    @report_group = report_group
+    @report_name = report_name
+    @params = string_to_hash(params)
+    @format = format
+    @partial_url = '/reports/%s/%s' % [@report_group, @report_name, TIME_ZONE_PARAMS]
+    @full_url = "#{BASE_URL}/#{@partial_url}"
+  end
 
-def to_ms(time)
-	start = Time.new(1970,1,1)
-	((time.to_f - start.to_f) * 1000.0).to_i
+  def build_xml_request
+    request_body = "<resourceDescriptor name='#{@report_name}' wsType='reportUnit' uriString='#{@partial_url}' isNew='false'><label>null</label>"
+    @params.each do |k, v|
+      request_body += "<parameter name='#{k}'>#{v}</parameter>"
+    end
+    request_body += '</resourceDescriptor>'
+  end
+
+  def get_uuid_and_cookie
+    body = ''
+    cookie = ''
+    uri = URI.parse(@full_url)
+    net_http = Net::HTTP.new(uri.host, uri.port)
+    net_http.read_timeout = TIMEOUT
+    net_http.start do |http|
+      req = Net::HTTP::Put.new(uri.path + "?RUN_OUTPUT_FORMAT=#{@format}")
+      req.basic_auth(SERVER_USER, SERVER_PASSWORD)
+      req.body = build_xml_request
+      resp = http.request(req)
+      body = resp.body
+      cookie = resp['Set-Cookie']
+    end
+    puts body
+
+    xml = REXML::Document.new(body)
+    uuid_xml = xml.elements['report/uuid']
+
+    if uuid_xml
+      uuid = uuid_xml.text
+      return uuid, cookie
+    else
+      return false, false
+    end
+  end
+
+  def get_file
+    body_get = nil
+    uuid, cookie = get_uuid_and_cookie
+    req = nil
+    if uuid
+      uri_get = URI.parse("#{BASE_URL}/#{uuid}")
+      http_get = Net::HTTP.new(uri_get.host, uri_get.port)
+      http_get.read_timeout = TIMEOUT
+      http_get.start do |http|
+        req = Net::HTTP::Get.new(uri_get.path + '?file=report')
+        req.basic_auth(SERVER_USER, SERVER_PASSWORD)
+        req['cookie'] = cookie
+        resp = http.request(req)
+        body_get = resp.body
+      end
+    else
+      raise JasperError, JasperError.request_error_message_for(@full_url)
+    end
+    body_get
+  end
+
+  private
+
+  def to_ms(time)
+    start = Time.new(1970, 1, 1)
+    ((time.to_f - start.to_f) * 1000.0).to_i
+  end
+
+  def string_to_hash(string)
+    if string.is_a? String
+      JSON.parse(string)
+    elsif string.is_a? Hash
+      string
+    else
+      raise JasperError, "Os parametros devem ser String(JSON) ou Hash e são #{string.class}"
+    end
+  end
 end
 
-#Examples of params, change for your report
-PARAMS = {:criacao_inicio=>to_ms(Time.new(2011,01,01)),:criacao_final=>to_ms(Time.new(2014,01,01))}
-
-
-def build_xml_request
-	url_string = "/reports/#{REPORT_GROUP}/#{REPORT_NAME}"
-	request_body = "<resourceDescriptor name='#{REPORT_NAME}' wsType='reportUnit' uriString='#{url_string}' isNew='false'><label>null</label>"
-	puts "creating url for : #{url_string} "
-	PARAMS.each do|k,v| 
-		request_body += "<parameter name='#{k}'>#{v.to_s}</parameter>"
-		puts "parameter send: #{k}=#{v.to_s}; "
-	end
-	request_body += "</resourceDescriptor>"
+class JasperError < StandardError
+  def self.request_error_message_for(url)
+    "Erro ao conectar com Jasper, verifique se o servidor está ativo,
+    se o relatório no Jasper está sendo gerado corretamente (diretamente na interface do Jasper) ou ainda
+    se o endereço do relatório está correto: #{url}"
+  end
 end
-
-def get_uuid_and_cookie
-
-	body = ""
-	cookie = ""
-	puts "FULL URL: #{"#{REPORT_PATH}/reports/#{REPORT_GROUP}/#{REPORT_NAME}"}"
-	uri = URI.parse("#{REPORT_PATH}/reports/#{REPORT_GROUP}/#{REPORT_NAME}")
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.start do |http|
-		req = Net::HTTP::Put.new(uri.path + "?RUN_OUTPUT_FORMAT=#{FORMAT}")
-		puts "send data for: #{uri.path}?RUN_OUTPUT_FORMAT=#{FORMAT}"
-		req.basic_auth(SERVER_USER, SERVER_PASSWORD)
-		req.body = build_xml_request
-		resp = http.request(req)
-		body = resp.body
-		puts "COOKIE received: #{resp['Set-Cookie']}"		
-		cookie = resp['Set-Cookie']
-	end
-
-	xml = REXML::Document.new(body)
-	puts "\n\n XML \n #{xml} \n\n"
-	uuid_xml = xml.elements["report/uuid"]
-	
-	if uuid_xml
-		uuid = uuid_xml.text
-		puts "UUID received: #{uuid}" 
-		return uuid,cookie
-	else
-		puts "Problem with UUID. Response: #{uuid_xml}" 
-		return false,false
-	end
-end
-
-def get_file
-	body_get = nil
-	uuid,cookie = get_uuid_and_cookie
-	if uuid
-		uri_get = URI.parse("#{REPORT_PATH}/#{uuid}")
-		puts "Report URL: #{uri_get}"
-		http_get = Net::HTTP.new(uri_get.host, uri_get.port)
-
-		http_get.start do |http|
-			req = Net::HTTP::Get.new(uri_get.path + "?file=report")		
-			puts "REQ: #{req.path}"
-			req.basic_auth(SERVER_USER, SERVER_PASSWORD)
-			req['cookie'] = cookie
-			resp = http.request(req)
-			body_get = resp.body		
-		end
-	end
-	body_get
-end
-
-
-file = get_file
-if file 
-	#puts file
-	f = File.new(FILE_NAME, 'wb')
-	f.write(file)
-	f.close
-else
-	puts "Ops, report file don't saved!"
-end	
